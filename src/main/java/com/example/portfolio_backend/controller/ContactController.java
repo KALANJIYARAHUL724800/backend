b9c13a34b690943c4ded669653da3f798a8bd54b
smartklrahul <smartklrahul@gmail.com>
2025-09-15 11:18:01 +0530

    new Changes

--- src/main/java/com/example/portfolio_backend/controller/ContactController.java
+++ src/main/java/com/example/portfolio_backend/controller/ContactController.java
@@ -7,9 +7,8 @@ import com.example.portfolio_backend.service.MessageInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-@CrossOrigin("*")
+
 @RestController
 @RequestMapping("api/contact")
 public class ContactController {
--- src/main/java/com/example/portfolio_backend/security/JwtFilter.java
+++ src/main/java/com/example/portfolio_backend/security/JwtFilter.java
@@ -1,4 +1,42 @@
 package com.example.portfolio_backend.security;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
+            if (JwtUtil.validateToken(jwt)) {
+                String username = JwtUtil.getUsername(jwt);
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                new User(username, "", new ArrayList<>()),
+                                null,
+                                new ArrayList<>()
+                        );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
